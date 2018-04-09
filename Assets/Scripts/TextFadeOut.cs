1f8d4c7fdecd0ac00e355bdbe7678654b2ebe0c0
aneshyba <annaneshyba@gmail.com>
2018-04-09 11:23:09 -0700

    Text Scripts

---
+++ Assets/Scripts/TextFadeOut.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+class TextFadeOut : MonoBehaviour
+{
+    //Fade time in seconds
+    public GameObject textGO; 
+    public float fadeOutTime;
+
+    // private start time
+    private int startTime;
+
+    public void Start()
+    {
+        startTime = System.DateTime.Now.Second; 
+    }
+
+
+    public void Update()
+    {
+        int timeDiff = System.DateTime.Now.Second - startTime;
+        if (timeDiff > 5 && timeDiff < 10)
+        {
+            FadeOut();
+        } 
+
+
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutRoutine());
+    }
+
+    private IEnumerator FadeOutRoutine()
+    {
+        TextMesh text = textGO.GetComponent<TextMesh>();
+        Color originalColor = text.color;
+        for (float t = 0.01f; t < fadeOutTime; t += Time.deltaTime)
+        {
+            text.color = Color.Lerp(originalColor, Color.clear, Mathf.Min(1, t / fadeOutTime));
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public GameObject text;
+    public int startTime; 
+
+	// Use this for initialization
+	void Start () {
+        startTime = System.DateTime.Now.Second;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int timeDiff = System.DateTime.Now.Second - startTime;
+        if (timeDiff > 6)
+        {
+            text.SetActive(false);
+        }
+    }
+}
