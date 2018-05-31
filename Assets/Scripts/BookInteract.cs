6d91cefa7bd8495ec8ed90000a5aad0aacff557c
aneshyba <annaneshyba@gmail.com>
2018-05-31 14:39:05 -0700

    Final Product
    This contains all of the final edits for the Missal environment.

---
+++ Assets/Scripts/BookInteract.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+using Valve.VR;
+
+namespace Valve.VR.InteractionSystem
+{ 
+
+    public class BookInteract : MonoBehaviour {
+
+        public Hand hand1;
+        public Hand hand2;
+
+        public GameObject closedBook;
+        public GameObject openBook;
+
+        private bool bookStatus; 
+
+        // Use this for initialization
+        void Start() {
+            bookStatus = true;
+            toggleBook(); 
+        }
+
+        public void OnTriggerStay(Collider other)
+        {
+            if (GameObject.ReferenceEquals(hand1.gameObject, other.gameObject))
+            {
+                Debug.Log("Hello hand1!");
+                if (hand1.controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+                {
+                    toggleBook(); 
+                }
+            }
+
+            if (GameObject.ReferenceEquals(hand2.gameObject, other.gameObject))
+            {
+                Debug.Log("Hello hand2!");
+                if (hand2.controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+                {
+                    toggleBook();
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        private void toggleBook()
+        {
+            bookStatus = !bookStatus;
+            openBook.SetActive(bookStatus);
+            closedBook.SetActive(!bookStatus);
+        }
+
+    }
+
+}
--- Assets/Scripts/ControllerControl.cs
+++ Assets/Scripts/ControllerControl.cs
@@ -2,99 +2,108 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerControl : MonoBehaviour {
+namespace Valve.VR.InteractionSystem
+{
 
-    // public game objects for interacting with 
-    public GameObject OpenBook;
-    public GameObject ClosedBook; 
-
-    private bool bookStatus;
-
-    // Controller and Collider object reference 
-    private SteamVR_TrackedObject trackedObj;
-    private GameObject collidingObject;
-    
-    // Use this for initialization
-    void Awake () {
-        bookStatus = true;
-
-        trackedObj = GetComponent<SteamVR_TrackedObject>();
-
-        toggleBook(); 
-	}
-
-    // Not sure what this does but whatever. 
-    private SteamVR_Controller.Device Controller
+    public class ControllerControl : MonoBehaviour
     {
-        get { return SteamVR_Controller.Input((int)trackedObj.index); }
-    }
+        
 
-    // Managing Collisions and triggers
-    private void SetCollidingObject(Collider col)
-    {
-        // 1
-        if (collidingObject || !col.GetComponent<Rigidbody>())
+        // public game objects for interacting with 
+        public GameObject OpenBook;
+        public GameObject ClosedBook;
+
+        private bool bookStatus;
+
+        // Controller and Collider object reference 
+        private SteamVR_TrackedObject trackedObj;
+        private GameObject collidingObject;
+
+        // Use this for initialization
+        void Awake()
         {
-            return;
-        }
-        // 2
-        collidingObject = col.gameObject;
-    }
+            bookStatus = true;
 
-    public void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("HELLOHELLOHELLOHELLOHELLO");
-        SetCollidingObject(other);
-    }
+            trackedObj = GetComponent<SteamVR_TrackedObject>();
 
-    // 2
-    public void OnTriggerStay(Collider other)
-    {
-        SetCollidingObject(other);
-    }
-
-    // 3
-    public void OnTriggerExit(Collider other)
-    {
-        if (!collidingObject)
-        {
-            return;
+            toggleBook();
         }
 
-        collidingObject = null;
-    }
-
-    // Book Functions 
-    void toggleBook()
-    {
-        bookStatus = !bookStatus; 
-        OpenBook.SetActive(bookStatus);
-        ClosedBook.SetActive(!bookStatus);
-    }
-
-    bool isBook(GameObject go )
-    {
-        if (GameObject.ReferenceEquals(go, OpenBook) || GameObject.ReferenceEquals(go, ClosedBook))
+        // Not sure what this does but whatever. 
+        private SteamVR_Controller.Device Controller
         {
-            Debug.Log("It is a book!!!");
-            return true;
-        } else
-        {
-            Debug.Log("It is NOT A BOOK");
-            return false; 
+            get { return SteamVR_Controller.Input((int)trackedObj.index); }
         }
-            
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Controller.GetHairTriggerDown())
+
+        // Managing Collisions and triggers
+        private void SetCollidingObject(Collider col)
         {
-            if (collidingObject)
+            // 1
+            if (collidingObject || !col.GetComponent<Rigidbody>())
             {
-                if (isBook(collidingObject))
+                return;
+            }
+            // 2
+            collidingObject = col.gameObject;
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("HELLOHELLOHELLOHELLOHELLO");
+            SetCollidingObject(other);
+        }
+
+        // 2
+        public void OnTriggerStay(Collider other)
+        {
+            SetCollidingObject(other);
+        }
+
+        // 3
+        public void OnTriggerExit(Collider other)
+        {
+            if (!collidingObject)
+            {
+                return;
+            }
+
+            collidingObject = null;
+        }
+
+        // Book Functions 
+        void toggleBook()
+        {
+            bookStatus = !bookStatus;
+            OpenBook.SetActive(bookStatus);
+            ClosedBook.SetActive(!bookStatus);
+        }
+
+        bool isBook(GameObject go)
+        {
+            if (GameObject.ReferenceEquals(go, OpenBook) || GameObject.ReferenceEquals(go, ClosedBook))
+            {
+                Debug.Log("It is a book!!!");
+                return true;
+            }
+            else
+            {
+                Debug.Log("It is NOT A BOOK");
+                return false;
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                if (collidingObject)
                 {
-                    toggleBook(); 
+                    if (isBook(collidingObject))
+                    {
+                        toggleBook();
+                    }
                 }
             }
         }
